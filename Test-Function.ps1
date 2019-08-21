c18aa1f65391eab8513c31cdb2533b86f63d1551
Mike Monaghan <mmonaghan@nextdigital.ca>
2019-08-21 16:37:28 -0600

    Commit

--- Test-Function.ps1
+++
@@ -1,4 +0,0 @@
-function Test-Function {
-        Get-Process A* | Select-Object -First 5 -ExpandProperty Name
-}
-Test-Function
--- WindowsUpdateAsJob.ps1
+++ WindowsUpdateAsJob.ps1
@@ -1,8 +1,4 @@
-﻿#changelog
-    # Added Send-SESMail functionality
-    # Revised Restart-Computer portion of script, adjusted to remotely use Restart-VM
-
-#GLOBAL VARIABLES. NO TOUCHY.
+﻿#GLOBAL VARIABLES. NO TOUCHY.
 $Jobs = @()
 $VerbosePreference = "Continue"
 
@@ -18,68 +14,69 @@ $FilePath = "C:\Scripts\Logs\$ScriptName\$Date.txt"
 
 #Begins transcription
 Start-Transcript -Path $FilePath -Append
-"$Date run of script beginning..."
+Write-Host "$Date run of script beginning..."
 
 #region Functions
 
 function Send-SESEmail {
-    [CmdletBinding()]
+[CmdletBinding()]
 
-    Param(
-        [Parameter(Mandatory)]
-        [string]$To,
+Param(
+    [Parameter(Mandatory)]
+    [string]$To,
 
-        [Parameter(Mandatory)]
-        [String]$Subject,
+    [Parameter(Mandatory)]
+    [String]$Subject,
 
-        [Parameter(Mandatory)]
-        [object]$Body,
+    [Parameter(Mandatory)]
+    $Body,
 
-        [Parameter()]
-        [String]$Attachment
-
-
-        )
-
-    begin {
-        $AWSSMTPUsername = "<ENTER USERNAME HERE>"
-        $AWSSMTPSecret = "<ENTER SECRET KEY HERE>"
+    [Parameter()]
+    [String]$Attachment
 
 
-        $SECURE_KEY = $(ConvertTo-SecureString -AsPlainText -String $AWSSMTPSecret -Force)
-        $creds = $(New-Object System.Management.Automation.PSCredential ($AWSSMTPUsername, $SECURE_KEY))
+    )
 
-        $Params = @{
-            To = $To
-            From = "<ENTER 'FROM' ADDRESS HERE>"
-            Subject = $Subject
-            Body = $Body
-            SmtpServer = "<ENTER AWS SES REGION URL HERE>"
-            Credential = $Creds
-            Port = 587
-            UseSSL = $True
-            }
-    }
+begin {
+    $AWSSMTPUsername = "<USERNAME>"
+    $AWSSMTPSecret = "<SECRET_KEY>"
 
-    process {
-        if ($Attachment) {
-            $Params.Attachments = $Attachment
-            }
+
+    $SECURE_KEY = $(ConvertTo-SecureString -AsPlainText -String $AWSSMTPSecret -Force)
+    $creds = $(New-Object System.Management.Automation.PSCredential ($AWSSMTPUsername, $SECURE_KEY))
+
+    $Params = @{
+        To = $To
+        From = "<FROM_ADDRESS>"
+        Subject = $Subject
+        Body = $Body
+        SmtpServer = "<AWS_SES_URL>"
+        Credential = $Creds
+        Port = 587
+        UseSSL = $True
         }
+}
 
-    end {
-        Send-MailMessage @Params
+process {
+    if ($Attachment) {
+        $Params.Attachments = $Attachment
         }
     }
+
+end {
+    Send-MailMessage @Params
+    }
+}
+
 #Check the status of the PSWindowsUpdate module and prompt for installation if it is not detected
 function Get-PSWindowsUpdateStatus {
     if (!(Get-InstalledModule "PSWindowsUpdate" -ErrorAction Ignore)) {
-        "PSWindowsUpdate is not installed on $env:COMPUTERNAME. Would you like to proceed with installation?"
+        Write-Host "PSWindowsUpdate is not installed on $env:COMPUTERNAME. Would you like to proceed with installation?"
         Install-Module -Name "PSWindowsUpdate" -AllowClobber -Force -Confirm
-        "PSWindowsUpdate has been installed on $env:COMPUTERNAME. Proceeding..."
+        Write-Host "PSWindowsUpdate has been installed on $env:COMPUTERNAME. Proceeding..."
          }
     else {
-        "PSWindowsUpdate is installed and up-to-date on $env:COMPUTERNAME. Proceeding..."
+        Write-Host "PSWindowsUpdate is installed and up-to-date on $env:COMPUTERNAME. Proceeding..."
          }
 }
 
@@ -98,7 +95,7 @@ foreach ($server in $Servers) {
 #Runs Get-AllUpdates function on all machines listed in $Servers as a job
 foreach ($server in $Servers) {
      $Jobs += (Invoke-Command -ComputerName $Server -ScriptBlock ${function:Get-AllUpdates})
-     "Job beginning on $Server"
+     Write-Host "Job beginning on $Server"
 }
 
 #Monitor the status of the Scheduled Tasks created by Invoke-WUJob cmdlet, add results to $RunTasks and $CompleteTasks. Refresh every 5 seconds until all tasks are out of Running state.
@@ -106,40 +103,41 @@ $runtime = 0
 DO{
     $runtime++
     $Elapsed = $runtime*5
-    "Starting loop $runtime. Elapsed time $Elapsed seconds"
+    Write-Host "Starting loop $runtime. Elapsed time $Elapsed seconds" -F Yellow
     $TaskStatus = Invoke-Command -ComputerName $Servers -ScriptBlock {Get-ScheduledTask -TaskName "PSWindowsUpdate"}
-    "Checking Task Status...`n"
+    Write-Host "Checking Task Status..."
     $RunTask = $TaskStatus | Where-Object {$_.state -eq "Running" -or $_.state -eq "4"}
     $CompleteTask = $TaskStatus | Where-Object {$_.state -eq "Ready" -or $_.state -eq "3"}
 
     "WAITING ON:"
     $RunTask | Select-Object PSComputerName,Description,State
-    "Refreshing..."
+
+    Write-Host "Refreshing..."
     Start-Sleep -seconds 5
     }
 UNTIL ($TaskStatus.state -notcontains "Running" -and $TaskStatus.state -notcontains "4" )
 
 #Pauses script and prompts user to review the results of $Jobs before proceeding with a keypress
-"PLEASE REVIEW THE ABOVE INFORMATION AND CONFIRM IT IS CORRECT BEFORE PROCEEDING"
+Write-Host "PLEASE REVIEW THE ABOVE INFORMATION AND CONFIRM IT IS CORRECT BEFORE PROCEEDING"
 $CompleteTask | Format-Table
-Send-SESEmail -To "<YOUR ADDRESS>"" -Subject 'Updates Complete, Reboot Pending' -Body 'Please press Enter on the script host to continue'
+Send-SESEmail -To mmonaghan@nextdigital.ca -Subject 'Updates Complete, Reboot Pending' -Body 'Please press Enter on the script host to continue'
 pause
 
 $RebootServers = $CompleteTask.PSComputername | Out-GridView -Passthru -Title "Select Servers to Reboot"
 
-"Rebooting $RebootServers"
+Write-Host "Rebooting $RebootServers"
 Invoke-Command -ComputerName $Host -ScriptBlock {$using:RebootServers | Restart-VM -Force -Confirm -Wait -For IPAddress}
-"Reboot Complete!"
+Write-Host "Reboot Complete!"
 
 $CompiledLogs = @()
 
 foreach ($server in $Servers) {
-    "Retrieving logs from " + $Server
+    Write-Host "Retrieving logs from " + $Server
     $CompiledLogs += Invoke-Command -ComputerName $Server -ScriptBlock {Get-Content C:\PSWindowsUpdate.log}
-    "Done!"
+    Write-Host "Done!"
     }
-"Compiling logs..."
+Write-Host "Compiling logs..."
 $CompiledLogs | Out-File C:\CompiledLogs.log
-"Done! See C:\CompiledLogs.log for details."
-"$Date run of script ended."
+Write-Host "Done! See C:\CompiledLogs.log for details."
+Write-Host "$Date run of script ended."
 Stop-Transcript
