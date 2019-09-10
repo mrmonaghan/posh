38b1b5809a7b7b1ca292b5af48f6b21525c2418d
Mike Monaghan <mmonaghan@nextdigital.ca>
2019-09-10 13:30:38 -0600

    Added back Error logging, added OverwriteExisting functionality

--- Copy-ADUserPermissions.ps1
+++ Copy-ADUserPermissions.ps1
@@ -11,6 +11,12 @@
  .Parameter To
   Selects the user or users to apply copied group membership to.
 
+ .Parameter OverwriteExisting
+  Removes user from all existing groups except for Domain Users before applying new membership.
+
+ .Parameter ShowErrors
+  Displays errors encountered by the cmdlet during the process block as part of the output.
+
  .Example
    # Copy permissions from UserA to UserB
    Copy-ADUserPermissions -From UserA -To UserB
@@ -18,8 +24,12 @@
  .Example
    # Copy permissions from UserA to UserB and UserC
    Copy-ADUserPermissions -From UserA -To UserB, UserC
+
+ .Example
+   # Copy permissions from UserA to UserB and remove UserB's previous group membership
+   Copy-ADUserPermissions -From UserA -To UserB -OverwriteExisting
 #>
-Function Copy-ADUserPermissons {
+Function Copy-ADUserPermissions {
     [CmdletBinding()]
 
     Param(
@@ -27,7 +37,13 @@ Function Copy-ADUserPermissons {
         [object]$From,
 
         [Parameter(Mandatory)]
-        [object]$To
+        [object]$To,
+
+        [Parameter()]
+        [switch]$OverwriteExisting,
+
+        [Parameter()]
+        [switch]$ShowErrors
 
     )
 
@@ -41,8 +57,23 @@ Function Copy-ADUserPermissons {
 
     }
     process {
+        if ($PSBoundParameters.ContainsKey('OverwriteExisting')) {
+            foreach ($User in $TargetUsersArray) {
+                Get-AdPrincipalGroupMembership -Identity $User | Where-Object -Property Name -Ne -Value 'Domain Users' | Remove-AdGroupMember -Members $User -confirm:$false
+            }
+        }
         foreach ($Group in $SourceGroups) {
-            Add-ADGroupMember -Identity $Group -Members $TargetUsersArray
+            try {
+                Add-ADGroupMember -Identity $Group -Members $TargetUsersArray
+            }
+            catch {
+                if ($PSBoundParameters.ContainsKey('ShowErrors')) {
+                    foreach ($Line in $Error) {
+                    Write-Error $Line
+                    }
+                }
+
+            }
         }
 
     }
@@ -55,9 +86,13 @@ Function Copy-ADUserPermissons {
             $TargetUserGroupMembership = Get-ADPrincipalGroupMembership $User
             $Comparison = Compare-Object -ReferenceObject $SourceGroups -DifferenceObject $TargetUserGroupMembership
             if (!($Comparison)) {
-                $UserResultObject.Status = 'Complete'
+                $UserResultObject.Status = 'Success'
                 }
-            else {$UserResultObject.Status = 'Failed'}
+            else {
+                $UserResultObject.Status = 'Error'
+                $UserResultObject | Add-Member -NotePropertyName 'DiffGroup' -NotePropertyValue $Comparison.InputObject.name
+                $UserResultObject | Add-Member -NotePropertyName 'Source/Target' -NotePropertyValue $Comparison.SideIndicator
+                }
             $ResultsArray.Add($UserResultObject)
         }
         Write-Output $ResultsArray
